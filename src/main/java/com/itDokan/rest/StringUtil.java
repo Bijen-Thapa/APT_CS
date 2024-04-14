bb9f64a37d034e9b22540e485fbd405e10896dd1
Bijen Thapa <bishal984060@gmail.com>
2024-04-14 15:22:05 +0545

    updated the dublicates
    dublicate of the StringUtil was created
    migrated the required contents and deleted the other one

--- src/main/java/com/itDokan/rest/StringUtil.java
+++ src/main/java/com/itDokan/rest/StringUtil.java
@@ -10,6 +10,16 @@ public class StringUtil {
 			+ "(id, name, image, img_path, description, price)"
 			+ "VALUES('', ?, ?, '', ?, ?)";
 	
+	public static final String GET_LOGIN_USER_INFO = "SELECT * FROM user_info WHERE user_name = ? AND password = ?";
 	
+	public static final String SUCCESS_MESSAGE = "successMessage";
+	
+	public static final String SUCCESS_REGISTER_MESSAGE ="Successfully registered";
+	
+	public static final String SERVER_ERROR_MESSAGE ="An unexpected server error occured";
+	
+	public static final String ERROR_MESSAGE = "error message";
+	
+	public static final String REGISTER_ERROR_MESSAGE = "Please correct the form data";
 	
 }
